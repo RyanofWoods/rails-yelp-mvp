fdbf7ed46b87423efabb9b8386da7bbf9d1747f3
Ryan Woods <contact@ryanofwoods.com>
2021-02-17 14:03:47 +0100

    Added validations for the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
   belongs_to :restaurant
 end
